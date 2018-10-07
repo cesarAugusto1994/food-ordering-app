52cb657695e2b7df30532b6bffebb2f30fdb59f0
Fausto95 <faustino.kialungila@gmail.com>
2018-10-07 22:04:40 +0200

    Refactored Form

--- src/components/Form.js
+++ src/components/Form.js
@@ -5,117 +5,35 @@ import {StyleSheet, Text, View, TouchableOpacity } from 'react-native';
 import {Mutation, graphql} from 'react-apollo';
 import t from 'tcomb-form-native';
 import gql from 'graphql-tag';
+import SaveButton from './Button'
 
 const Form = t.form.Form;
 
-const Person = t.struct({
-  name: t.String,
-  description: t.String,
-  location: t.String,
-  waitTime: t.Number,
-  speciality: t.String,
-  image: t.String
-});
-
-export default class _Form extends React.Component {
-  state = {
-    value: {
-      name: '',
-      description: '',
-      location: '',
-      waitTime: 0,
-      speciality: '',
-      image: ''
-    }
-  }
-
-  onChange = (value) => {
-    this.setState({value})
-  }
-
-  mutate  = (e, mutationFn) => {
-    e.preventDefault();
-    const {
-      props: {
-        restaurantId,
-        ownerId,
-        mutationName,
-        edit
-      },
-      state:{ value }
-    } = this;
-    const variables = {
-      ...value,
-      restaurantId,
-      ownerId
-    };
-
-    mutationFn({
-      variables,
-      optimisticResponse: () => ({
-        [mutationName]: { ...variables, __typename: 'Restaurant' },
-        __typename: 'Mutation'
-      }, {}),
-    })
-    .then(data => console.log('post', {data}))
-    .catch(err => console.error({err}))
-  }
-  render() {
-    const {
-      type,
-      options,
-      color,
-      value = this.state.value,
-      mutation,
-      text,
-      alert,
-    } = this.props;
-    return (
-      <Mutation mutation={mutation}>
-        {(mutationFn, {client, data, error, loading}) => (
-          <View style={styles.container}>
-            {/* display */}
-            <Form
-              type={Person}
-              options={options}
-              onChange={this.onChange}
-              value={value}
-            />
-            <TouchableOpacity
-              style={[styles.button, {backgroundColor: color, borderColor: color}]}
-              onPress={
-                async e => await this.mutate(e, mutationFn)
-              }
-            >
-              <Text style={styles.buttonText}>{text}</Text>
-            </TouchableOpacity>
-          </View>
-        )}
-      </Mutation>
-    )
-  }
-};
+export default ({ type, options, value, onSave, text, iconName, onChange, formStyle }) => (
+  <View style={[styles.container, formStyle]}>
+    <View style={styles.form}>
+      <Form { ...{type, options, value, onChange}}/>
+    </View>
+    <SaveButton
+      onPress={onSave} { ...{text, iconName}}
+      buttonStyle={styles.buttonStyle}/>
+  </View>
+)
 
 const styles = StyleSheet.create({
   container: {
     justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 50,
-    padding: 20,
-    backgroundColor: '#ffffff',
+    padding: 5,
+    backgroundColor: '#fff',
   },
-  buttonText: {
-    fontSize: 18,
-    color: 'white',
-    alignSelf: 'center'
+  form: {
+    justifyContent: 'center',
+    width: '100%',
+    padding: 5
   },
-  button: {
-    height: 36,
-    backgroundColor: '#48BBEC',
-    borderColor: '#48BBEC',
-    borderWidth: 1,
-    borderRadius: 8,
-    marginBottom: 10,
-    alignSelf: 'stretch',
-    justifyContent: 'center'
+  buttonStyle: {
+    marginLeft: 0
   }
 });
--- src/owner/NewRestaurant.js
+++ src/owner/NewRestaurant.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import Form from '../components/Form';
+import CreateRestaurant from '../components/CreateAndEditRestaurant';
 import gql from 'graphql-tag';
 import {Text, ScrollView, TouchableOpacity, Alert, View} from 'react-native';
 import uuidv4 from 'uuid/v4';
+import FlashMessage from 'react-native-flash-message';
 
 import {colors} from '../theme';
 import {CREATE_RESTAURANTE} from '../graphql/owner/';
@@ -16,15 +17,15 @@ export default connect(mapToPros, actions)(({navigation: {getParam, goBack}, use
   return (
     <View style={{flex: 1, backgroundColor: 'white'}}>
       <ScrollView>
-        <Form
-          color={colors.primary}
+        <CreateRestaurant
           ownerId={ownerId}
           restaurantId={restaurantId}
           mutation={CREATE_RESTAURANTE}
           mutationName='createrestaurant'
-          alert={() => {}}
+          goBack={goBack}
           text="Adicionar"
         />
+        <FlashMessage position='top'/>
       </ScrollView>
     </View>
   )
